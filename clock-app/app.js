a5d984f76ef3be8fc20265f99189154a3b377139
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-07-09 16:58:53 +0300

    lesson #23

--- clock-app/app.js
+++ clock-app/app.js
@@ -5,8 +5,7 @@ function TimeController($scope) {
     var currentDate = new Date();
     $scope.timeString = currentDate.toTimeString();
     $scope.userName = "";
-    
-    
+        
     $scope.updateTime = function() {
         //$scope.userName = "";
         var currentDate = new Date();
