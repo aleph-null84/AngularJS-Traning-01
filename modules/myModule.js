281bfe3934150cde2a0f5b7be7f53dd8df29bba0
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-07-13 20:26:13 +0300

    lesson #32

---
+++ modules/myModule.js
@@ -0,0 +1,9 @@
+var myHelloModuleVar = angular.module("myHelloModule", []);
+
+
+myHelloModule.controller("HelloCtrl", HelloCtrl);
+
+function HelloCtrl() {
+    this.helloMessage = "I'm from the myHelloModule module";
+
+}
